23b94d2b1151407f03f01b9e153648d48fc45564
kynmh69 <hiroki.kayanuma@gmail.com>
2024-10-13 21:49:31 +0900

    #2 add user domain

--- go/src/cmd/users/main.go
+++ go/src/cmd/users/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	_ "github.com/kynmh69/mormorare/docs"
 	"github.com/kynmh69/mormorare/internal/app"
 	"github.com/kynmh69/mormorare/internal/database"
 	"github.com/kynmh69/mormorare/internal/domain"
 	"github.com/kynmh69/mormorare/pkg/logging"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func init() {
@@ -12,6 +15,25 @@ func init() {
 	logging.Initialize()
 }
 
+//	@title			Swagger Example API
+//	@version		1.0
+//	@description	This is a sample server celler server.
+//	@termsOfService	http://swagger.io/terms/
+
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+//	@host		localhost:8080
+//	@BasePath	/api/v1
+
+//	@securityDefinitions.basic	BasicAuth
+
+// @externalDocs.description	OpenAPI
+// @externalDocs.url			https://swagger.io/resources/open-api/
 func main() {
 	// initialize the database
 	db := database.NewPostgres()
@@ -23,5 +45,6 @@ func main() {
 	// initialize the application
 	r := app.NewEngine(db)
 	r.Route()
+	r.Engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Run()
 }
