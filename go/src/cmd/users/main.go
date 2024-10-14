7fc036caa39d7603c905fa07a247010fe99b19ef
kynmh69 <hiroki.kayanuma@gmail.com>
2024-10-14 16:54:05 +0900

    #2 add user domain

--- go/src/cmd/users/main.go
+++ go/src/cmd/users/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	_ "github.com/kynmh69/mormorare/docs"
+	"github.com/kynmh69/mormorare/internal/domain"
+	"github.com/kynmh69/mormorare/internal/wire"
 	"github.com/kynmh69/mormorare/pkg/logging"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func init() {
@@ -14,15 +18,15 @@ func init() {
 // @version 1.0
 // license.name MIT
 func main() {
-	//	// initialize the application
-	//	r := app.InitializeEngine()
-	//	// initialize the database
-	//	if err := r.Db.AutoMigrate(&domain.User{}); err != nil {
-	//		logging.GetLogger().Panicln(err)
-	//	} else {
-	//		logging.GetLogger().Info("Database migrated")
-	//	}
-	//	r.Route()
-	//	r.Engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	//	r.Run()
+	// initialize the application
+	r := wire.InitializeEngine()
+	// initialize the database
+	if err := r.Db.AutoMigrate(&domain.User{}); err != nil {
+		logging.GetLogger().Panicln(err)
+	} else {
+		logging.GetLogger().Info("Database migrated")
+	}
+	r.Route()
+	r.Engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Run()
 }
